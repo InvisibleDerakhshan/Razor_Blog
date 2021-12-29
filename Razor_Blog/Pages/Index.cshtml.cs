34fb761fba3366ab1a72af62d6a3dbdd4cfaed93
InvisibleDerakhshan <87549134+InvisibleDerakhshan@users.noreply.github.com>
2021-12-29 16:38:16 +0330

    Add project files.

---
+++ Razor_Blog/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Razor_Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_Blog.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<ArticleViewModel> Articles { get; set; }
+        private readonly BlogContext _context;
+
+        public IndexModel(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            Articles = _context.Articles.Select(x => new ArticleViewModel()
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                ShortDescription = x.ShortDescription,
+                
+            }).OrderByDescending(x=>x.Id).ToList();
+        }
+
+        //    public IActionResult OnGetLoad()
+        //    {
+        //        return RedirectToPage("./Index");
+        //        //return Page();
+        //}
+
+        //public void Onpost(IFormCollection form)
+        //{
+
+        //}
+    }
+}
